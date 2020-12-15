aaa7976426c5fbed112fc51bbaa61dc5815fc9e3
cobacdavid <7426305+cobacdavid@users.noreply.github.com>
2020-12-15 20:59:06 +0100

    poo wa

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # abrviz
+
+## usage
+
+``` python
+import random
+    
+    
+liste = list(range(20))
+random.shuffle(liste)
+
+a = Arbre()
+# a.set_fonction_ordre(lambda x, y: str(x) < str(y))
+
+for i in liste:
+    a.ajoute_noeud(i)
+
+a.sortie("test", "png")
+```
+
+## Exemple de sortie image
+
+<img src="test.png" width="800">
+
+## Licence
+CC-BY-NC-SA
--- abrviz.py
+++ abrviz.py
@@ -1,9 +1,8 @@
 __author__ = "david cobac"
 __date__ = 20201215
 
-
 import graphviz
-import random
+
 
 def est_plus_petit(element1, element2, fonction=None):
     if not fonction: fonction = lambda a, b: a < b
@@ -13,13 +12,19 @@ def est_plus_petit(element1, element2, fonction=None):
 class Arbre:
     def __init__(self):
         self.liste = []
+        self.fonction_ordre = None
+        self.graphe = graphviz.Digraph(engine="dot")
+
+    def set_fonction_ordre(self, fonction):
+        self.fonction_ordre = fonction
 
     def ajoute_noeud(self, valeur):
         indice = 1
         trouve = False if self.liste else True 
 
         while not trouve:
-            if est_plus_petit(self.liste[indice - 1], valeur):
+            if est_plus_petit(self.liste[indice - 1], valeur,
+                              self.fonction_ordre):
                 indice = 2 * indice + 1
             else:
                 indice = indice * 2
@@ -29,52 +34,59 @@ class Arbre:
             self.liste += [None] * (indice - len(self.liste))
         self.liste[indice - 1] = valeur
 
-
-def visu(arbre, nom_fichier, format):
-    g = graphviz.Digraph(engine="dot")
-    g.attr("node", shape="record")
-    g.attr("edge", headport="n")
-    g.attr("edge", tailclip="false")
-    #
-    visunoeud(g, arbre.liste, 1)
-    g.render(nom_fichier, format=format)
+    def sortie(self, nom_fichier, format):
+        self.graphe.attr("node", shape="record")
+        self.graphe.attr("edge", headport="n")
+        self.graphe.attr("edge", tailclip="false")
+        #
+        self._visunoeud(self.graphe, 1)
+        self.graphe.render(nom_fichier, format=format)
 
 
-def visunoeud(graphe, liste, indice):
-    contenu = liste[indice - 1]
-    if contenu is None: return
+    def _visunoeud(self, graphe, indice):
+        contenu = self.liste[indice - 1]
+        if contenu is None: return
+
+        N = len(self.liste)
+
+        indice_gauche = 2 * indice
+        indice_droit = indice_gauche + 1
+        contenug = self.liste[indice_gauche - 1] if indice_gauche <= N else None
+        contenud = self.liste[indice_droit - 1] if indice_droit <= N else None
+
+        with graphe.subgraph(name=str(contenu) + "sub") as gsub:
+            gsub.node(str(contenu), label="{" + str(contenu)+ "|{<g>|<d>}}")
+            if contenug is None:
+                gsub.node(str(contenu) + "invisg", style="invis")
+                gsub.edge(str(contenu) + ":g:c", str(contenu) + "invisg",
+                            style="invis", weight="10")
+            else:
+                gsub.edge(str(contenu) + ":g:c", str(contenug))
+
+            if contenud is None:
+                gsub.node(str(contenu) + "invisd", style="invis")
+                gsub.edge(str(contenu) + ":d:c", str(contenu) + "invisd",
+                            style="invis", weight="10")
+            else:
+                gsub.edge(str(contenu) + ":d:c", str(contenud))
+
+            if indice_gauche - 1 < N:
+                self._visunoeud(gsub, indice_gauche)
+            if indice_droit - 1 < N:
+                self._visunoeud(gsub, indice_droit)
+
+
+
+if __name__ == "__main__":
+    import random
     
-    contenug = liste[2*indice - 1] if 2 * indice <= len(liste) else None
-    contenud = liste[2*indice] if 2 * indice + 1 <= len(liste) else None
+    
+    liste = list(range(20))
+    random.shuffle(liste)
 
-    graphe.node(str(contenu), label="{" + str(contenu)+ "|{<g>|<d>}}")
+    a = Arbre()
+    # a.set_fonction_ordre(lambda x, y: str(x) < str(y))
+    for i in liste:
+        a.ajoute_noeud(i)
 
-    if contenug is None:
-        graphe.node(str(contenu) + "invisg", style="invis")
-        graphe.edge(str(contenu) + ":g:c", str(contenu) + "invisg",
-                    style="invis", weight="-100")
-    else:
-        graphe.edge(str(contenu) + ":g:c", str(contenug), weight="100")
-        
-    if contenud is None:
-        graphe.node(str(contenu) + "invisd", style="invis")
-        graphe.edge(str(contenu) + ":d:c", str(contenu) + "invisd",
-                    style="invis", weight="-100")
-    else:
-        graphe.edge(str(contenu) + ":d:c", str(contenud), weight="100")
-
-    if 2*indice - 1 < len(liste):
-        visunoeud(graphe, liste, 2 * indice)
-    if 2*indice < len(liste):
-        visunoeud(graphe, liste, 2*indice + 1)
-
-
-
-liste = list(range(20))
-random.shuffle(liste)
-
-a = Arbre()
-for i in liste:
-    a.ajoute_noeud(i)
-
-visu(a, "test", "png")
+    a.sortie("test", "png")
